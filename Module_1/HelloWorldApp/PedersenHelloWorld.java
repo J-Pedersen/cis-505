702608fa45dbd0bef3725650ea2187a29ced793b
Jeff <jmpedersen@my365.bellevue.edu>
2025-03-11 19:52:18 -0500

    added initial module folder and helloworld folder and java files

---
+++ Module_1/HelloWorldApp/PedersenHelloWorld.java
@@ -0,0 +1,9 @@
+public class PedersenHelloWorld {
+    public static void main(String[] args) {
+
+        String firstName = "Jeff";
+        String lastName = "Pedersen";
+
+        System.out.println("Hello World from " + firstName + " " + lastName + ".");
+    }
+}
